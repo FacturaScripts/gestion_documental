dcc571fd726df3ed08685352a8dbdcdf618a5e41
Joel <joel@fusiodarts.com>
2016-08-11 17:10:41 +0200

    Implementada la paginación

--- controller/gestion_documental.php
+++ controller/gestion_documental.php
@@ -31,6 +31,21 @@ class gestion_documental extends fs_controller
     public $offset;
     public $b_adjunto;
     public $filtros;
+    public $options;
+    
+    public $docs;
+    public $desde;
+    public $hasta;
+    public $adj;
+    public $url;
+    
+    public $pages;
+    
+    public $facturacli;
+    
+    public $desc_tipodoc;
+    public $tipodoc;
+    public $extension;
 
     public function __construct()
     {
@@ -40,7 +55,7 @@ class gestion_documental extends fs_controller
     protected function private_core()
     {
 
-//        $this->facturacli  = new factura_cliente();
+        $this->facturacli  = new factura_cliente();
 //        $this->facturaprov = new factura_proveedor();
 //        $this->albarancli  = new albaran_cliente();
 //        $this->albaranprov = new albaran_proveedor();
@@ -48,11 +63,6 @@ class gestion_documental extends fs_controller
         $this->gesdoc   = new gestion_documento();
 //        $this->fdesde = Date('1-m-Y');
 //        $this->fhasta = Date('d-m-Y');
-//        $this->offset      = 0;
-//        if (isset($_REQUEST['offset']))
-//        {
-//            $this->offset = intval($_REQUEST['offset']);
-//        }
 //
 //        /// primer nivel de ordenación
 //        $this->order = 'fecha DESC';
@@ -89,44 +99,170 @@ class gestion_documental extends fs_controller
 //        }
         // Asignamos los tipos de documentos para el selector
         $this->tiposdoc = $this->gesdoc->tipos_documentos();
+        
+        $this->url = $this->url();
 
-
+        $this->offset = 0;
+        if (isset($_REQUEST['offset'])) {
+            $this->offset = intval($_REQUEST['offset']);
+        }
+        
+        $this->docs = '';
+        $this->desde = '';
+        $this->hasta = '';
+        $this->adj = '';
 
         // Inicializamos los filtros
+        if (isset($_POST['tipodoc']) && $_POST['tipodoc'] != '')
+        {
+            $this->gesdoc->set_filtros('tipodoc', $_POST['tipodoc']);
+        } else if (isset($_GET['docs']) && $_GET['docs'] != '')
+        {
+            $this->gesdoc->set_filtros('tipodoc', $_POST['docs']);
+        } else
+        {
+            $this->gesdoc->set_filtros('tipodoc', 'FC');
+            $this->docs = 'FC';
+        }
+        
+        $sql = "";
+        $where = '';
         if (isset($_POST['b_adjunto']) && $_POST['b_adjunto'] != '')
         {
             $this->gesdoc->set_filtros('b_adjunto', $_POST['b_adjunto']);
+        } else if (isset($_GET['adjunto']) && $_GET['adjunto'] != '') 
+        {
+            $this->gesdoc->set_filtros('b_adjunto', $_GET['adjunto']);
         }
         if (isset($_POST['desde']) && $_POST['desde'] != '')
         {
             $this->gesdoc->set_filtros('b_fdesde', $_POST['desde']);
-        }
+            $d = date('Y-m-d', strtotime($_POST['desde']));
+            $sql .= $where . " f.fecha >= '" . $d . "'";
+            $where = ' AND ';
+        } else if (isset($_GET['desde']) && $_GET['desde'] != '')
+        {
+            $this->gesdoc->set_filtros('b_fdesde', $_GET['desde']);
+            $d = date('Y-m-d', strtotime($_GET['desde']));
+            $sql .= $where . " f.fecha >= '" . $d . "'";
+            $where = ' AND ';
+        }        
         if (isset($_POST['hasta']) && $_POST['hasta'] != '')
         {
             $this->gesdoc->set_filtros('b_fhasta', $_POST['hasta']);
+            $h = date('Y-m-d', strtotime($_POST['hasta']));
+            $sql .= $where . " f.fecha <= '" . $h . "'";
+        } else if (isset($_GET['hasta']) && $_GET['hasta'] != '')
+        {
+            $this->gesdoc->set_filtros('b_fhasta', $_GET['hasta']);
+            $h = date('Y-m-d', strtotime($_GET['hasta']));
+            $sql .= $where . " f.fecha <= '" . $h . "'";
         }
+        
         $this->filtros = $this->gesdoc->filtros;
+        $this->options = '';
+        
+        if (isset($_REQUEST['tipodoc'])) {
+            $this->docs = $_REQUEST['tipodoc'];
+        } else if (isset($_GET['docs'])) {
+            $this->docs = $_GET['docs'];
+        }
+        
+        if (isset($_GET['desde'])) {
+            $this->desde = $_GET['desde'];
+        } else {
+            $this->desde = $this->filtros['b_fdesde'];
+        }
+        
+        if (isset($_GET['hasta'])) {
+            $this->hasta = $_GET['hasta'];
+        } else {
+            $this->hasta = $this->filtros['b_fhasta'];
+        }
+        
+        if (isset($_GET['adjunto'])) {
+            $this->adj = $_GET['adjunto'];
+        } else {
+            $this->adj = $this->filtros['b_adjunto'];
+        }
 
         // Mostramos listado por tipo de documento
-        if (isset($_POST['tipodoc']) && $_POST['tipodoc'] != '')
+        if (isset($_POST['tipodoc']) && $_POST['tipodoc'] != '' || isset($_GET['docs']) && $_GET['docs'] != '')
         {
             $this->tipodoc = $_POST['tipodoc'];
-            if ($this->tipodoc == 'FC')
-            {
-                $this->resultados = $this->gesdoc->all_facturas_clientes();
-            } else if ($this->tipodoc == 'FP')
-            {
-                $this->resultados = $this->gesdoc->all_facturas_proveedores();
-            } else if ($this->tipodoc == 'AC')
-            {
-                $this->resultados = $this->gesdoc->all_albaranes_clientes();
-            } else if ($this->tipodoc == 'AP')
-            {
-                $this->resultados = $this->gesdoc->all_albaranes_proveedores();
-            }
         } else
         {
-            $this->resultados = $this->gesdoc->all_documentos();
+            $this->tipodoc = 'FC';
+        }
+        
+        if ($this->tipodoc == 'FC' || $this->docs == 'FC')
+        {
+            $this->options .= '&facturacli=true';
+            $this->desc_tipodoc = 'Factura Cliente';
+            $tabla = 'facturascli';
+            $id = 'idfactura';
+        } else if ($this->tipodoc == 'FP' || $this->docs == 'FP')
+        {
+            $this->options .= '&facturaprov=true';
+            $this->desc_tipodoc = 'Factura Proveedor';
+            $tabla = 'facturasprov';
+            $id = 'idfactura';
+        } else if ($this->tipodoc == 'AC' || $this->docs == 'AC')
+        {
+            $this->options .= '&albarancli=true';
+            $this->desc_tipodoc = 'Albarán Cliente';
+            $tabla = 'albaranescli';
+            $id = 'idalbaran';
+        } else if ($this->tipodoc == 'AP' || $this->docs == 'AP')
+        {
+            $this->options .= '&albaranprov=true';
+            $this->desc_tipodoc = 'Albarán Proveedor';
+            $tabla = 'albaranesprov';
+            $id = 'idalbaran';
+        }
+        
+        if ($this->adj != '0') {
+            $wh = '';
+            if ($sql) {
+                $wh = ' AND ' . $sql;
+            }
+        } else {                
+            $wh = '';
+            if ($sql) {
+                $wh = ' WHERE ' . $sql;
+            }
+        }
+        
+        if ($this->tipodoc) {
+            $doc = $this->tipodoc;
+        } else if ($this->docs) {
+            $doc = $this->docs;
+        }
+        
+        if ($this->adj == '1') {
+            $sql_res = 'documentosfac as d, '.$tabla.' as f WHERE f.'.$id.' = d.'.$id.' ';
+            $sql_pages = ', documentosfac as d WHERE f.'.$id.' = d.'.$id.' ';           
+        } else if ($this->adj == '2') {
+            $sql_res = $tabla.' as f LEFT JOIN documentosfac as d ON f.'.$id.' = d.'.$id.' WHERE d.'.$id.' IS NULL ';
+            $sql_pages = ' LEFT JOIN documentosfac as d ON f.'.$id.' = d.'.$id.' WHERE d.'.$id.' IS NULL ';
+        } else {
+            $sql_res = $tabla.' as f LEFT JOIN documentosfac as d ON d.'.$id.' = f.'.$id.' ';
+            $sql_pages = '';
+        }
+        
+        $res = $this->gesdoc->get_documents($sql, $sql_res, $sql_pages, $this->offset, $doc, $this->adj, $tabla, $id);
+        $this->resultados = $res[0];
+        $this->pages = $res[1];
+        
+        foreach ($this->resultados as $i=>$r) {
+            $this->resultados[$i]['extension'] = substr(strrchr($r['doc_nombre'], '.'), 1);
+        }
+        
+        if ($this->filtros) {
+            $this->url .= '&docs=' . $this->docs;
+            $this->url .= '&desde=' . $this->desde;
+            $this->url .= '&hasta=' . $this->hasta;
+            $this->url .= '&adjunto=' . $this->adj;
         }
 
         $returned = $this->get_data('http://fusioerp.local/index.php?page=plantillas_pdf&factura=TRUE&id=1917');
@@ -136,7 +272,6 @@ class gestion_documental extends fs_controller
     }
 
     /* gets the data from a URL */
-
     public function get_data($url)
     {
         $ch      = curl_init();
@@ -147,6 +282,53 @@ class gestion_documental extends fs_controller
         $data    = curl_exec($ch);
         curl_close($ch);
         return $data;
+    }
+   
+    public function paginas()
+    {
+        $url = $this->url;
+
+        $paginas = array();
+        $i = 0;
+        $num = 0;
+        $actual = 1;
+        $total = intval($this->pages[0]['total']);
+//        $total = count($this->resultados);
+
+        /// añadimos todas la página
+        while ($num < $total) {
+            $paginas[$i] = array(
+                'url' => $url . "&offset=" . ($i * FS_ITEM_LIMIT),
+                'num' => $i + 1,
+                'actual' => ($num == $this->offset)
+            );
+
+            if ($num == $this->offset) {
+                $actual = $i;
+            }
+
+            $i++;
+            $num += FS_ITEM_LIMIT;
+        }
+
+        /// ahora descartamos
+        foreach ($paginas as $j => $value) {
+            $enmedio = intval($i / 2);
+
+            /**
+             * descartamos todo excepto la primera, la última, la de enmedio,
+             * la actual, las 5 anteriores y las 5 siguientes
+             */
+            if (($j > 1 AND $j < $actual - 5 AND $j != $enmedio) OR ( $j > $actual + 5 AND $j < $i - 1 AND $j != $enmedio)) {
+                unset($paginas[$j]);
+            }
+        }
+        
+        if (count($paginas) > 1) {
+            return $paginas;
+        } else {
+            return array();
+        }
     }
 
 //
--- model/gestion_documento.php
+++ model/gestion_documento.php
@@ -50,8 +50,7 @@ class gestion_documento extends fs_model
     
     public function __construct($gd = FALSE)
     {
-
-//        parent::__construct('gesdoc');
+        parent::__construct();
 //        $facturacli  = new factura_cliente();
 //        $facturaprov = new factura_proveedor();
 //        $albarancli  = new albaran_cliente();
@@ -176,34 +175,34 @@ class gestion_documento extends fs_model
     }
     
 
-    public function all_documentos()
+    public function all_documentos($offset)
     {
-        $all = array_merge($this->all_facturas_clientes(), $this->all_albaranes_clientes(), $this->all_facturas_proveedores(), $this->all_albaranes_proveedores());
+        $all = array_merge($this->all_facturas_clientes($offset), $this->all_albaranes_clientes($offset), $this->all_facturas_proveedores($offset), $this->all_albaranes_proveedores($offset));
         return $all;
     }
     
-    public function all_facturas_clientes()
+    public function all_facturas_clientes($offset)
     {
         $model = new factura_cliente();
-        return $this->all_by_model($model);
+        return $this->all_by_model($model, $offset);
     }
 
-    public function all_facturas_proveedores()
+    public function all_facturas_proveedores($offset)
     {
         $model = new factura_proveedor();
-        return $this->all_by_model($model);
+        return $this->all_by_model($model, $offset);
     }
 
-    public function all_albaranes_clientes()
+    public function all_albaranes_clientes($offset)
     {
         $model = new albaran_cliente();
-        return $this->all_by_model($model);
+        return $this->all_by_model($model, $offset);
     }
 
-    public function all_albaranes_proveedores()
+    public function all_albaranes_proveedores($offset)
     {
         $model = new albaran_proveedor();
-        return $this->all_by_model($model);
+        return $this->all_by_model($model, $offset);
     }
 
     /**
@@ -211,17 +210,19 @@ class gestion_documento extends fs_model
      * @param type $model
      * @return \gestion_documento
      */
-    protected function all_by_model($model)
+    protected function all_by_model($model, $offset=null)
     {
-        $fdesde = $this->filtros['fdesde'];
-        $fhasta = $this->filtros['fhasta'];
+        $fdesde = $this->filtros['b_fdesde'];
+        $fhasta = $this->filtros['b_fhasta'];
         
         if ($fdesde != '' && $fdesde != null && $fhasta != '' && $fhasta != null)
         {
+            $fdesde = date('Y-m-d', strtotime($fdesde));
+            $fhasta = date('Y-m-d', strtotime($fhasta));
             $listado = $model->all_desde($fdesde, $fhasta);
         } else
         {
-            $listado = $model->all();
+            $listado = $model->all($offset, FS_ITEM_LIMIT);
         }
         
         $resultados = array();
@@ -345,6 +346,21 @@ class gestion_documento extends fs_model
     public function save()
     {
         
+    }
+    
+    public function get_documents($sql, $sql_res, $sql_pages, $offset, $tipodoc, $adjunto, $tabla, $id)
+    {
+        $wh = '';
+        if ($sql && adjunto == '1' || $sql && $adjunto == '2') {
+            $wh = ' AND ' . $sql;
+        } else if ($sql != '' && $adjunto == '0') {
+            $wh = ' WHERE ' . $sql;
+        }
+        
+        $resultados = $this->db->select_limit("SELECT f.*, d.*, d.nombre as doc_nombre, d.fecha as doc_fecha, d.hora as doc_hora FROM " . $sql_res . $wh . " ORDER BY f.fecha DESC, f.codigo DESC ", FS_ITEM_LIMIT, $offset);
+        $pages = $this->db->select("SELECT COUNT(f.".$id.") as total FROM ".$tabla." as f " . $sql_pages . $wh . ";");
+
+        return array($resultados, $pages);
     }
 
 }
